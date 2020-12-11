34f3065c120d534b03bab648553e03c2386d699b
ELIANE DOS REIS CABRAL DA ROCHA <75854127+elianecabral@users.noreply.github.com>
2020-12-11 17:04:12 -0300

    Laço de Repetição While - Primeiro Exercício
    Sequência de números inteiros de 1 a 100 que fizemos em aula.

---
+++ contagem.java
@@ -0,0 +1,23 @@
+package contagem;
+import java.util.Scanner;
+public class contagem {
+	public static void main (String [] Args) {
+
+	
+	
+	
+		 Scanner in= new Scanner(System.in);
+	       int i;
+	        i = 1;
+	        
+	        while (i<=100) {
+	            
+	            System.out.println("Realizar contagem de 1 a 100:\n"+i);
+	            
+	             i++;
+	        }
+	    }
+	    
+	}
+	
+	
